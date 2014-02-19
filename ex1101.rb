bd97f417c704925c9a1a53c3ce7bb3d81983eef6
jtaka1012 <simple.gadget.life@gmail.com>
2014-02-19 21:59:37 +0900

    p152まで完了

---
+++ ex1101.rb
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+friends = {
+	:shin => "Shin Kuboaki",
+	:shinichirou => "Shinichirou Ooba",
+	:shingo => "Shingo Katori"
+}
+
+friends.each{ |key,value|
+	puts "#{key} #{value}"
+}
---
+++ ex1102.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+friends = {
+	:shin => "Shin Kuboaki",
+	:shinichirou => "Shinichirou Ooba",
+	:shingo => "Shingo Katori"
+}
+
+friends.each_key{ |key|
+	puts "#{key}"
+}
